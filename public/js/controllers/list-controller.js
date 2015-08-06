9bddf0eef16b9e157c0b6eea2cb89b9bc2567b85
Constantino <Coproductions@Constantins-MacBook-Pro.local>
2015-08-06 09:41:57 -1000

    comment number displayed on list view

--- public/js/controllers/list-controller.js
+++ public/js/controllers/list-controller.js
@@ -13,40 +13,51 @@ angular.module('myApp')
       $scope.hello = 'goodbye';
 
       LinkService.getAllLinks('created_at','dsc').then(function(response){
-        $scope.allLinks = response;
+        $scope.allLinks = response.data;
+        addCommentNr($scope.allLinks);
       })
+
+      function addCommentNr(arrayOfLinks){
+        arrayOfLinks.forEach(function(link){
+          LinkService.getComments(link._id)
+            .then(function(res){
+              link.commentNr = res.data.length;
+            })
+        })
+      }
 
       $scope.addLink = function(){
         var newLink = $('#url-input').val()
          LinkService.addLink(newLink, function(response){
-          $scope.allLinks.data.unshift(response.data);
+          $scope.allLinks.unshift(response.data);
          });
       }
 
       $scope.sortByRating = function(){
         console.log('sorting')
         LinkService.getAllLinks('rating','dsc').then(function(response){
-        $scope.allLinks = response;
+        $scope.allLinks = response.data;
+        addCommentNr($scope.allLinks);
         })
       }
 
       $scope.sortByDate = function(){
         console.log('sorting')
         LinkService.getAllLinks('created_at','dsc').then(function(response){
-        $scope.allLinks = response;
+        $scope.allLinks = response.data;
+        addCommentNr($scope.allLinks);
         })
       }
 
       $scope.deleteLink = function(id){
         LinkService.deleteLink(id);
-        $scope.allLinks.data = $scope.allLinks.data.filter(function(link){
+        $scope.allLinks = $scope.allLinks.filter(function(link){
           return link._id !== id;
         })
       }
 
       $scope.nrOfComments = function(id){
         LinkService.getComments(id).then(function(res){
-
           console.log('comment nr:',res.data.length)
           $scope.commentNr = res.data.length;
         });
